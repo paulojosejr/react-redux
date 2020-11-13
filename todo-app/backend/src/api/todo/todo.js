799f2fd1b3b06b33a2644989b6feecee26bfab62
Paulo Araújo <paulojosejr95@gmail.com>
2020-11-13 10:51:21 +0000

    mapping routes and testing api (postman)

--- todo-app/backend/src/api/todo/todo.js
+++ todo-app/backend/src/api/todo/todo.js
@@ -1,10 +1,10 @@
 const restful = require('node-restful')
 const mongoose = restful.mongoose
 
-const todoSchema = new mongoose.schema ({
+const todoSchema = new mongoose.Schema ({
     description: {type: String, required: true},
     done: { type: Boolean, required: true, default: false},
-    CreateAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now }
 })
 
 module.exports = restful.model('Todo', todoSchema)
