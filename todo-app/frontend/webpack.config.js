3f675b82ece1e9a9e6dae314593a110be41d2b32
Paulo Araújo <paulojosejr95@gmail.com>
2020-11-17 23:31:27 +0000

    create app index

--- todo-app/frontend/webpack.config.js
+++ todo-app/frontend/webpack.config.js
@@ -8,19 +8,19 @@ module.exports = {
         filename: './app.js'
     },
     devServer: {
-        port: 8081,
+        port: 8080,
         contentBase: './public',
     },
     resolve: {
         extensions: ['', '.js', '.jsx'],
         alias: {
-            modules: __dirname + 'node_modules'
+            modules : __dirname + '/node_modules'
         }
     },
-    plugins: [
+    puglins: [
         new ExtractTextPlugin('app.css')
     ],
-    module: {
+    modules: {
         loaders: [{
             test: /.js[x]?$/,
             loader: 'babel-loader',
@@ -33,7 +33,7 @@ module.exports = {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         }, {
-            test: /\.woff|.woff2|.ttf.|.eot|.svg*.*$/,
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
             loader: 'file'
         }]
     }
