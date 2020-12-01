a9e6177f3aacc67e91c0d2ee1f9a4ff6c23698b9
Paulo Araújo <paulojosejr95@gmail.com>
2020-12-01 22:48:58 +0000

    React-router navigation

---
+++ navigation/src/views/examples/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = props => (
+    <div className='About'>
+        <h1>About</h1>
+        <p>Our system has been created...</p>
+    </div>
+)
+export default About
